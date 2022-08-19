110b8eb2303a752d0a17acad7f1b66781d9d7b34
Shahid Iqbal <shahid.iqbal4213@gmail.com>
2022-08-19 16:43:54 +0500

    Update 1.0.3

--- app/UtilityMagic/src/main/java/com/gulehri/utilsmagic/Utilities.kt
+++ app/UtilityMagic/src/main/java/com/gulehri/utilsmagic/Utilities.kt
@@ -63,16 +63,25 @@ class Utilities() {
     object Internet {
         @RequiresApi(Build.VERSION_CODES.M)
         fun isConnected(context: Context): Boolean {
-            val connectivityManager =
-                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+          
+            
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  
+                val network = connectivityManager.activeNetwork ?: return false
+                val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
 
-            val network = connectivityManager.activeNetwork ?: return false
-            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-            return when {
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                else -> false
+                return when {
+                  
+                    activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    else -> false
+                }
+            } else {
+                @Suppress("DEPRECATION") val networkInfo =
+                    connectivityManager.activeNetworkInfo ?: return false
+                @Suppress("DEPRECATION")
+                return networkInfo.isConnected
             }
         }
     }
